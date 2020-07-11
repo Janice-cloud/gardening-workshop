d573ff86edff5942fe9e2d86caf8857a5e875819
Janice Leung <janice.leung2008@gmail.com>
2020-07-12 00:26:42 +1000

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const projects = require('./routes/api/projects')
 
@@ -17,6 +18,16 @@ mongoose
 
 //Use Routes
 app.use('/api/projects', projects)
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+     });
+}
 
 const PORT = process.env.PORT || 5000;
 
