aecb5b087dd33a21d889b184820bb40814087f7e
Janice Leung <janice.leung2008@gmail.com>
2020-07-30 16:20:21 +1000

    add links

--- client/src/components/BrandDescription/BrandDescription.js
+++ client/src/components/BrandDescription/BrandDescription.js
@@ -11,25 +11,25 @@ class BrandDescription extends Component {
                     <div className="cardBackground col-xs-12 text-center">
                         <div className="card-body">
                             <h5 className="card-title text-light">Follow Us!</h5>
-                            <a href="/">
+                            <a href="https://www.facebook.com/janice.leung.731135" target="_blank">
                                 <span className="fa-stack facebookLogo">
                                     <i className="far fa-circle fa-stack-2x"></i>
                                     <i className="fab fa-facebook-f fa-stack-1x"></i>
                                 </span>
                             </a>
-                            <a href="/">
+                            <a href="https://twitter.com/GardeningWorks1" target="_blank">
                                 <span className="fa-stack twitterLogo">
                                     <i className="far fa-circle fa-stack-2x"></i>
                                     <i className="fab fa-twitter fa-stack-1x"></i>
                                 </span>
                             </a>
-                            <a href="/">
+                            <a href="https://www.instagram.com/gardeningworkshop/" target="_blank">
                                 <span className="fa-stack instagramLogo">
                                     <i className="far fa-circle fa-stack-2x"></i>
                                     <i className="fab fa-instagram fa-stack-1x"></i>
                                 </span>
                             </a>
-                            <a href="/">
+                            <a href="https://www.linkedin.com/in/gardening-workshop-3187691b3/" target="_blank">
                                 <span className="fa-stack linkedinLogo">
                                     <i className="far fa-circle fa-stack-2x"></i>
                                     <i className="fab fa-linkedin-in fa-stack-1x"></i>
